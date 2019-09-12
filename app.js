4392795fbfa27aa6dad48b2cf289b0d7be8d80b5
Annie Elizabeth Freeman <annie.e.freeman@gmail.com>
2019-09-12 19:38:19 +1200

    Add sign up page and functionality

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+//jshint esversion:6
+
 // Require the .env file (this will contain secret information - TBC)
 require('dotenv').config();
 
 // Require packages
 const express = require("express");
 const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const request = require("request");
 
 // Set up express
 const app = express(); // Create the express application -> named 'app'
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true })); // Allows parsing of requests (POST)
 app.use(express.static("public")); // Serve static files from the 'public' directory
 
@@ -26,7 +31,95 @@ app.get("/contact", function(req, res) {
 
 app.get("/jobs", function(req, res) {
     res.render("jobs");
-})
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/signup", function(req, res){
+  res.render("signup");
+});
+
+app.get("/success", function(req, res){
+  res.render("success");
+});
+
+app.get("/failure", function(req, res){
+  res.render("failure");
+});
+
+//Set up post request for when user inputs their details
+app.post("/signup", function(req, res){
+
+//Parse values from form
+firstName = req.body.firstName;
+lastName = req.body.lastName;
+email = req.body.email;
+// age = req.body.age;
+//region they are from
+
+//Put values into one variable called userInformation
+console.log(firstName, lastName, email);
+
+var userInformation = {
+  members: [
+    {
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+//age and region
+    }
+  ]
+};
+
+//Save into a new variable that converts the userInformation into a JSON object
+var jsonData = JSON.stringify(userInformation);
+
+var options = {
+  url: "https://us4.api.mailchimp.com/3.0/lists/8776ede861",
+  method: "POST",
+  headers: {
+    "Authorization": "aefreeman API KEY GOES HERE" //--> remember to change end to the server allocated
+  },
+  body: jsonData
+
+};
+
+
+//Render success or failure template
+
+request(options, function(error, response, body){
+
+if(error){
+  res.redirect("/failure");
+} else if (response.statusCode != 200) {
+
+    res.redirect("/failure");
+  } else {
+    res.redirect("/success");
+  }
+
+});
+
+//Go to login page after success page
+app.post("/success", function(req, res){
+  res.redirect("/login");
+});
+
+//Return to sign up page from failure page when button clicked using a post request
+
+app.post("/failure", function(req, res){
+  res.redirect("/signup");
+});
+
+});
+
+
+
 
 // Set up port
 // Run the server on port 3000, unless it is deployed (Heroku sets process.env.PORT)
