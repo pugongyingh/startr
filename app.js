2f9f7f1d3ff73f38342322b244f03262d429d9b9
Annie Elizabeth Freeman <annie.e.freeman@gmail.com>
2019-09-28 20:40:16 +1200

    Add local auth

--- app.js
+++ app.js
@@ -9,25 +9,53 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const request = require("request");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+// const GoogleStrategy = require('passport-google-oauth20').Strategy; //used as a passport strategy
+// const findOrCreate = require("mongoose-findorcreate");
+// const FacebookStrategy = require("passport-facebook").Strategy;
 
 // Set up express
 const app = express(); // Create the express application -> named 'app'
+// app.set('view engine', 'ejs');
+
+
+
+// app.use(express.urlencoded({
+//   extended: true
+// })); // Allows parsing of requests (POST)
+// app.use(express.static("public")); // Serve static files from the 'public' directory
+
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
-
-//Avoid Mongoose deprecation warnings
-app.set('useFindAndModify', false);
-
-app.use(express.urlencoded({
+app.use(bodyParser.urlencoded({
   extended: true
-})); // Allows parsing of requests (POST)
-app.use(express.static("public")); // Serve static files from the 'public' directory
+}));
+
+
+
+app.use(session({
+  secret: process.env.PASSPORT_SECRET, //transfer to .env file
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session()); //use passport to deal with the sessions (set up before)
+
 
 //Connect to MongoDB database (db is created here)
-mongoose.connect("mongodb://localhost:27017/interviewsDB", {
+mongoose.connect("mongodb://localhost:27017/startrDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+//Avoid Mongoose deprecation warnings
+app.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+///////////////////////INTERVIEW COLLECTION//////////////////////////
 //Create a new schema
 const interviewSchema = {
   video: String,
@@ -40,19 +68,36 @@ const interviewSchema = {
 //Create a new mongoose model based on the schema
 const Interview = mongoose.model("Interview", interviewSchema);
 
-//////////TEST: Create new object////////
-// const newInterview = new Interview({
-//   video: '<iframe width="400" height="300" src="https://www.youtube.com/embed/HVJ7VaKfcqQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-//   title: "Physio: Murray Hing",
-//   blurb: "Murray Hing blurb",
-//   content:"Long form content",
-//   soundCloud:"will figure out soon",
+///////////////////////USER LOGINS COLLECTION//////////////////////////
+//Create a new schema
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+//Use passport plugins
+userSchema.plugin(passportLocalMongoose);
+
+//Create a new mongoose model based on the schema
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+// passport.serializeUser(function(user, done) {
+//   done(null, user.id);
 // });
 //
-// newInterview.save();
+// passport.deserializeUser(function(id, done) {
+//   User.findById(id, function(err, user) {
+//     done(err, user);
+//   });
+// });
 
-// Set up routes
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
+///////////////////////SET UP ROUTES//////////////////////////
 app.get("/", function(req, res) {
   res.render("home");
 });
@@ -66,20 +111,42 @@ app.get("/contact", function(req, res) {
 });
 
 app.get("/jobs", function(req, res) {
-    Interview.find({}, function(err, foundInterviews) {
-      if (!err) {
-        res.render("jobs", {
-          interviews: foundInterviews
-        });
-      } else {
-        console.log(err);
-      }
-
-    });
-
-
+  Interview.find({}, function(err, foundInterviews) {
+    if (!err) {
+      res.render("jobs", {
+        interviews: foundInterviews
+      });
+    } else {
+      console.log(err);
+    }
+  });
 });
 
+//Route to display single interview
+app.get("/interviews/:interviewID", function(req, res) {//not finding the ID
+  const requestedInterviewID = req.params.interviewId;
+  console.log(requestedInterviewID);
+  Interview.findOne({ //this is not finding the interview
+    _id: requestedInterviewID
+  }, function(err, interview) {
+
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(interview);
+      res.render("singleInterview");
+    //   , {
+    //   //   video: interview.video,
+    //   //   title: interview.title,
+    //   //   content: interview.content,
+    //   //   soundCloud: interview.soundCloud
+    //   // });
+    //
+    // }
+
+  }
+});
+});
 
 app.get("/login", function(req, res) {
   res.render("login");
@@ -98,7 +165,17 @@ app.get("/failure", function(req, res) {
 });
 
 app.get("/edit", function(req, res) {
+  //authenticate user: only if they have admin login
   res.render("edit");
+});
+
+app.get("/dashboard", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("dashboard");
+  } else {
+    res.redirect("/login");
+  }
+
 });
 
 //Set up post request for when user inputs their details
@@ -107,11 +184,15 @@ app.post("/signup", function(req, res) {
   //Parse values from form
   firstName = req.body.firstName;
   lastName = req.body.lastName;
-  email = req.body.email;
+  email = req.body.username;
+  password = req.body.password;
   // age = req.body.age;
   //region they are from
 
-  //Put values into one variable called userInformation
+
+  //////////Send user information to mailchimp//////////////
+
+  // Put values into one variable called userInformation
   console.log(firstName, lastName, email);
 
   var userInformation = {
@@ -126,33 +207,51 @@ app.post("/signup", function(req, res) {
     }]
   };
 
-  //Save into a new variable that converts the userInformation into a JSON object
+  // Save into a new variable that converts the userInformation into a JSON object
   var jsonData = JSON.stringify(userInformation);
-
+  const mailChimpAPIKey = process.env.MAILCHIMP_API_KEY;
   var options = {
     url: "https://us4.api.mailchimp.com/3.0/lists/8776ede861",
     method: "POST",
     headers: {
-      "Authorization": "aefreeman API KEY GOES HERE" //--> remember to change end to the server allocated
+      "Authorization": "aefreeman mailChimpAPIKey" //--> remember to change end to the server allocated
     },
     body: jsonData
 
   };
 
-  //Render success or failure template
+  // Render success or failure template
 
-  request(options, function(error, response, body) {
+  // request(options, function(error, response, body) {
+  //
+  //   if (error) {
+  //     res.redirect("/failure");
+  //   } else if (response.statusCode != 200) {
+  //
+  //     res.redirect("/failure");
+  //   } else {
+  //     res.redirect("/success");
+  //   }
+  //
+  // });
 
-    if (error) {
-      res.redirect("/failure");
-    } else if (response.statusCode != 200) {
-
+  /////////////Create user in database/////////////////////////
+  User.register({
+    username: req.body.username
+  }, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
       res.redirect("/failure");
     } else {
-      res.redirect("/success");
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/success");
+        console.log("Success");
+      });
     }
-
   });
+  console.log(res.statusCode);
+
+
 
   //Go to login page after success page
   app.post("/success", function(req, res) {
@@ -168,20 +267,25 @@ app.post("/signup", function(req, res) {
 });
 
 
-// // Encode/decode htmlentities
-const youtubeEmbedCode = '<iframe width="560" height="315" src="https://www.youtube.com/embed/vHfxfocDdBU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-const encodedCode = encodeURIComponent(youtubeEmbedCode);
-const decodedCode = decodeURIComponent(encodedCode);
+app.post("/login", function(req, res) {
 
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/dashboard");
+      });
 
-// 	function krEncodeEntities(s){
-// 		return $j("<div/>").text(s).html();
-// 	}
-// 	function krDencodeEntities(s){
-// 		return $j("<div/>").html(s).text();
-// 	}
+    }
+  });
 
+});
 
 //////////////// DB requests using edit page //////////////////
 
@@ -229,10 +333,11 @@ app.post("/edit", function(req, res) {
       });
   } else if (methodType === "delete") {
 
-    Interview.deleteOne(
-      {title: req.body.currentTitle},
-      function(err){
-        if(!err){
+    Interview.deleteOne({
+        title: req.body.currentTitle
+      },
+      function(err) {
+        if (!err) {
           res.redirect("/edit");
           console.log("Successfully deleted item from database");
         } else {
@@ -244,8 +349,6 @@ app.post("/edit", function(req, res) {
 
 
   } else {
-
-
     //Change this to an alert!
     console.log("Error with editing the interviews page");
   }
