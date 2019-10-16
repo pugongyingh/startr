fffbd6996c83bca928a76327d20bd25070205d08
Annie Elizabeth Freeman <annie.e.freeman@gmail.com>
2019-10-16 19:02:58 +1300

    Add other routes for ejs to html

--- build.js
+++ build.js
@@ -34,3 +34,38 @@ ejs2html({
   path: `${__dirname}/views/about.ejs`,
   outPath: `${__dirname}/public/about.html`,
 });
+
+ejs2html({
+  path: `${__dirname}/views/jobs.ejs`,
+  outPath: `${__dirname}/public/jobs.html`,
+});
+
+ejs2html({
+  path: `${__dirname}/views/contact.ejs`,
+  outPath: `${__dirname}/public/contact.html`,
+});
+
+ejs2html({
+  path: `${__dirname}/views/signup.ejs`,
+  outPath: `${__dirname}/public/signup.html`,
+});
+
+ejs2html({
+  path: `${__dirname}/views/edit.ejs`,
+  outPath: `${__dirname}/public/edit.html`,
+});
+
+ejs2html({
+  path: `${__dirname}/views/oneInterview.ejs`,
+  outPath: `${__dirname}/public/oneInterview.html`,
+});
+
+ejs2html({
+  path: `${__dirname}/views/failure.ejs`,
+  outPath: `${__dirname}/public/failure.html`,
+});
+
+ejs2html({
+  path: `${__dirname}/views/success.ejs`,
+  outPath: `${__dirname}/public/success.html`,
+});
